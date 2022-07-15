602ddc5965bd17e41a34de1d9e65fad62515cdf1
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2022-07-15 15:53:11 -0300

    Update and rename primos.c to verificacao_primo.c

---
+++ verificacao_primo.c
@@ -0,0 +1,15 @@
+int verificacao_primo (unsigned int n) {
+    
+    //exige math.h
+    
+    if (n < 2) return 0;
+    if (n == 2) return(1);
+    if (n % 2 == 0) return(0);
+
+    for(int i = 3; i <= (unsigned int)sqrt((double)n)+1; i += 2) {
+        if (n % i == 0) 
+            return 0;
+    } 
+    
+    return 1;
+}
