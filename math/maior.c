f550b29501c6f902a6eac9fc0adcf48d69d81775
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2022-11-03 13:37:52 -0300

    Delete maior.c

--- math/maior.c
+++
@@ -1,7 +0,0 @@
-int maiorAB(int a, int b) {
-    return (a + b + abs(a - b)) / 2;
-}
-
-int maiorABC(int a, int b, int c) {
-    return (2 * c + a + b + abs(a - b) + abs(-2 * c + a + b + abs(a - b))) / 4;
-}
