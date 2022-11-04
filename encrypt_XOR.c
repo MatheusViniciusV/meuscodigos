f615e62d348661fd8149f87b6f58f89a9efd4beb
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2022-11-04 18:44:51 -0300

    Delete encrypt_XOR.c

--- encrypt_XOR.c
+++
@@ -1,8 +0,0 @@
-int encrypt_XOR_int(int a, int b) {
-    return a ^ b;
-}
-
-void encrypt_XOR_string(char *array, int senha) {
-    for(int i = 0; array[i] != '\0'; i++)
-        array[i] ^= senha;
-}
