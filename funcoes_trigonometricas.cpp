20190625cb3a64735d53bd761393bb9801876d75
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2022-11-05 11:34:13 -0300

    Update funcoes_trigonometricas.cpp

--- funcoes_trigonometricas.cpp
+++ funcoes_trigonometricas.cpp
@@ -1,28 +1,35 @@
-#include <cmath> //fmod
+include <cmath> //fmod
 
-#define LIMIT 30 //Controla o número de polinômios na aproximação
+#define LIMITE 27 //Controla o número de polinômios nas aproximações
 
-//Cálculo semelhante das funções seno e cosseno
-double taylor(double x, int i, double bg)  { 
-    
-    double r = bg, t = bg;
- 
-    for(; i < LIMIT; i += 2) //Série de Taylor
-        r += t = -t*x*x/(i*(i-1));         
- 
-    return r; 
+double rad(int graus) { //Conversão de graus para radianos
+    return M_PI / 180 * graus; 
 }
 
 double seno(double x)  {
+    
     x = fmod(x,M_PI*2); //Desconsidera as voltas inteiras no círculo trigonométrico
-    return taylor(x, 3, x); 
+    
+    double r = x, t = x;
+ 
+    for(int i = 3; i < LIMITE; i += 2) //Série de Taylor na forma da função seno
+        r += t = -t*x*x/(i*(i-1));   
+    
+    return r; 
 }
 
 double cosseno(double x)  { 
+    
     x = fmod(x,M_PI*2);
-    return taylor(x, 2, 1);
+    
+    double r = 1, t = 1;
+ 
+    for(int i = 2; i < LIMITE; i += 2) //Série de Taylor na forma da função cosseno
+        r += t = -t*x*x/(i*(i-1));     
+    
+    return r;
 }
 
 double tangente(double x) {
-    return sin(x)/cos(x); //Identidade trigonométrica
+    return seno(x)/cosseno(x); //Identidade trigonométrica
 }
