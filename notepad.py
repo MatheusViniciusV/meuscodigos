7d32f6f678702fc023b37953edf94d72e5a631c0
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2022-12-22 15:53:25 -0300

    Update notepad.py

--- notepad.py
+++ notepad.py
@@ -8,73 +8,72 @@ WHITE = '#ffffff'
 
 class Notepad:
 
-    root = Tk()
-    textBox = Text(root)
-    scrollbar = Scrollbar(textBox)
-    menuBar = Menu(root)
-    archiveMenu = Menu(menuBar, tearoff=0)
-    styleMenu = Menu(menuBar, tearoff=0)
-    helpMenu = Menu(menuBar, tearoff=0)
-    file = None
+    __root = Tk()
+    __textBox = Text(__root)
+    __scrollbar = Scrollbar(__textBox)
+    __menuBar = Menu(__root)
+    __archiveMenu = Menu(__menuBar, tearoff=0)
+    __file = None
 
     def __init__(self):
 
-        self.root.title('Untitled - Notepad')
-        self.root.geometry('400x400')
-        self.root.resizable(False, False)
-        self.root.config(menu=self.menuBar)
-        self.root.grid_rowconfigure(0, weight=1)
-        self.root.grid_columnconfigure(0, weight=1)
+        self.__root.title('Untitled - Notepad')
+        self.__root.geometry('400x400')
+        self.__root.resizable(False, False)
+        self.__root.config(menu=self.__menuBar)
+        self.__root.grid_rowconfigure(0, weight=1)
+        self.__root.grid_columnconfigure(0, weight=1)
 
-        self.scrollbar.pack(side=RIGHT, fill=Y)
-        self.scrollbar.config(command=self.textBox.yview) 
+        self.__scrollbar.pack(side=RIGHT, fill=Y)
+        self.__scrollbar.config(command=self.__textBox.yview) 
 
-        self.textBox.grid(sticky = N + E + S + W)
-        self.textBox.config(yscrollcommand=self.scrollbar.set)
-        self.textBox.config(bg=BLACK, fg=WHITE, font='Arial 12 bold')
+        self.__textBox.grid(sticky = N + E + S + W)
+        self.__textBox.config(yscrollcommand=self.__scrollbar.set)
+        self.__textBox.config(bg=WHITE, fg=BLACK, font='Arial 12')
 
-        self.archiveMenu.add_command(label='New', command=self.new)
-        self.archiveMenu.add_command(label='Open', command=self.open)
-        self.archiveMenu.add_command(label='Save', command=self.save)
-        self.archiveMenu.add_command(label='Exit', command=self.exit)
-        self.menuBar.add_cascade(label='Archive', menu=self.archiveMenu)
+        self.__archiveMenu.add_command(label='New', command=self.__new)
+        self.__archiveMenu.add_command(label='Open', command=self.__open)
+        self.__archiveMenu.add_command(label='Save', command=self.__save)
+        self.__archiveMenu.add_command(label='Exit', command=self.__exit)
+        self.__menuBar.add_cascade(label='Archive', menu=self.__archiveMenu)
 
-    def new(self):
-        self.file = None
-        self.textBox.delete(1.0, 'end')
+    def __new(self):
+        self.__file = None
+        self.__textBox.delete(1.0, 'end')
+        self.__root.title("Untitled - Notepad") 
 
-    def open(self):
+    def __open(self):
 
-        self.file = askopenfilename(defaultextension = '.txt',
+        self.__file = askopenfilename(defaultextension = '.txt',
             filetypes = [('Todos os Arquivos','*.*'),
                         ('Arquivos de texto','*.txt')])
-        if self.file == "": 
-            self.file = None
+        if self.__file == "": 
+            self.__file = None
         else: 
-            self.root.title(os.path.basename(self.file) + " - Notepad") 
-            self.textBox.delete(1.0,END) 
-            file = open(self.file,"r") 
-            self.textBox.insert(1.0,file.read()) 
-            file.close()
+            self.__root.title(os.path.basename(self.__file) + " - Notepad") 
+            self.__textBox.delete(1.0,END) 
+            __file = open(self.__file,"r") 
+            self.__textBox.insert(1.0,__file.read()) 
+            __file.close()
 
-    def save(self):
+    def __save(self):
 
-        if self.file is None:
-            self.file = asksaveasfilename(initialfile = 'arquivo.txt',
+        if self.__file is None:
+            self.__file = asksaveasfilename(initial__file = 'arquivo.txt',
             defaultextension = '.txt',
             filetypes = [('Todos os Arquivos','*.*'),
             ('Arquivos de Texto','*.txt')])
 
-        self.root.title(os.path.basename(self.file) + " - Notepad") 
-        file = open(self.file,'w')
-        file.write(self.textBox.get(1.0,'end'))
-        file.close()
+        self.__root.title(os.path.basename(self.__file) + " - Notepad") 
+        __file = open(self.__file,'w')
+        __file.write(self.__textBox.get(1.0,'end'))
+        __file.close()
 
-    def exit(self): 
-        self.root.destroy() 
+    def __exit(self): 
+        self.__root.destroy() 
 
     def run(self): 
-        self.root.mainloop()
+        self.__root.mainloop()
 
 main = Notepad()
 main.run()
