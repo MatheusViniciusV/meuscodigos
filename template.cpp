2356bb3df5a5409978fc1d0e6b4ab9d6ed721df2
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2024-01-17 14:43:49 -0300

    Create template.cpp

---
+++ template.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define fastios std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
+#define dbg(x) cout << #x << " = " << x << endl
+#define endl "\n"
+#define ll long long 
+
+int main()
+{
+    fastios
+
+    
+
+    return 0;
+}
