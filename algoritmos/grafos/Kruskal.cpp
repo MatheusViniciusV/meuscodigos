a7ece23bed398883e04744275c01815fe882d10e
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2024-02-04 23:44:19 -0300

    Create Kruskal.cpp

---
+++ algoritmos/grafos/Kruskal.cpp
@@ -0,0 +1,33 @@
+struct Aresta {
+    int origem, destino, peso;
+    inline bool operator<(Aresta outra) { return peso < outra.peso; }
+};
+
+typedef vector<Aresta> Grafo;
+
+Grafo original, AGM;
+int* pai;
+
+int encontrar_conjunto(int i) {
+    return (i != pai[i]) ? i = encontrar_conjunto(pai[i]) : i;
+}
+
+void unir_conjuntos(int u, int v) {
+    pai[u] = pai[v];
+}
+
+void Kruskal() {
+    
+    Grafo copia = original;
+    
+    sort(copia.begin(), copia.end()); //ordena pelos pesos
+    
+    for (Aresta A : copia) {
+        int conjunto_origem = encontrar_conjunto(A.origem);
+        int conjunto_destino = encontrar_conjunto(A.destino);
+        if (conjunto_origem != conjunto_destino) {
+            AGM.push_back(A); 
+            unir_conjuntos(conjunto_origem, conjunto_destino);
+        }
+    }
+}
