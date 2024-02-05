db8fce4ed02019d02f78775bb5db1747a7563287
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2024-02-05 00:16:26 -0300

    Create DFS.cpp

---
+++ algoritmos/grafos/DFS.cpp
@@ -0,0 +1,15 @@
+//Objetivo: Busca em um grafo todos os nós acessíveis de um nó raiz
+//Complexidade: O(V + A)
+
+const int MAX = 1e3+10;
+
+vector<bool> visitado(MAX, false);
+vector<vector<int>> adjacencias(MAX);
+
+void DFS(int vertice) {
+    
+    visitado[vertice] = true;
+    
+    for(auto vizinho : adjacencias[vertice])
+        if(!visitado[vizinho]) DFS(vizinho);
+}
