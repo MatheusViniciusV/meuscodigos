0951106c897e4c3f0dba7f2537c29e60b1584bbb
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2024-02-10 19:00:36 -0300

    Update template.cpp

--- algoritmos/template.cpp
+++ algoritmos/template.cpp
@@ -4,14 +4,25 @@ using namespace std;
 
 #define fastios std::ios::sync_with_stdio(false); cin.tie(NULL); cout.tie(NULL);
 #define dbg(x) cout << #x << " = " << x << endl
-#define endl "\n"
-#define ll long long 
+#define loop(i, a, n) for(int i = a; i < n; i++)
 
-int main()
+typedef long long ll;
+
+//const int MAX = 1e3+0;
+//const ll INF = 0x3f3f3f3f3f3f3f3f; 
+
+/*struct Estrutura {
+    int x, y, z;
+    inline bool operator<(const Estrutura& other) const { return x < other.x; }
+}*/
+
+//vector<vector<int>> matriz(M, vector<int>(N, 0));
+
+int32_t main()
 {
     fastios
 
     
-
+    
     return 0;
 }
