dc0855c951825d8b2823d5f8924dd1d5b43d8c94
MatheusViniciusV <101892449+MatheusViniciusV@users.noreply.github.com>
2024-04-05 17:48:08 -0300

    Create Kadane.cpp

---
+++ algoritmos/Kadane.cpp
@@ -0,0 +1,19 @@
+//Objetivo: Busca a maior soma contígua em um array
+//Complexidade: O(N)
+
+#define INF = 0x3f3f3f3f;
+
+vector<int> arr;
+
+int kadane() {
+  int max_soma = -INF;
+  int soma = 0;
+
+  for(auto e : bingos) {
+    soma += e;
+    max_soma = max(max_soma, soma);
+    soma = max(soma, 0);
+  }
+
+  return max_soma;
+}
